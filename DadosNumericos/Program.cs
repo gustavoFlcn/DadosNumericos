5e6337c8181ae01c219118b319bf8fb26df10006
Gustavo Feliciano <gustavo.flcn@gmail.com>
2022-09-09 21:41:21 -0300

    Describe the sum function

--- DadosNumericos/Program.cs
+++ DadosNumericos/Program.cs
@@ -2,6 +2,8 @@
 // Description: Create a calculator to calculate the basic operations
 
 class Calc{
+
+    // This function make and print a sum of two numbers
     public sum(){
         Console.WriteLine(1+1);
     }
