f78b7fa316d81b630bebdeebca3ac24516e48db5
Gustavo Feliciano <gustavo.flcn@gmail.com>
2022-09-09 21:23:42 -0300

    Add a function that calculate a sum

--- DadosNumericos/Program.cs
+++ DadosNumericos/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Author: Gustavo Feliciano Figueiredo
+// Description: Create a calculator to calculate the basic operations
 
+class Calc{
+    public sum(){
+        Console.WriteLine(1+1);
+    }
+}
