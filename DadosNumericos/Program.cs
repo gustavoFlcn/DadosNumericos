20aa46bc8079bbdd158db6c877b503c459d39fbf
Gustavo Feliciano <gustavo.flcn@gmail.com>
2022-09-09 22:19:24 -0300

    Merge with master

--- DadosNumericos/Program.cs
+++ DadosNumericos/Program.cs
@@ -11,8 +11,4 @@ class Calc{
     public void subtraction(int number1, int number2){
         Console.WriteLine($"Your result: {number1} + {number2} = " + (number1 + number2));
     }
-
-    public void subtraction(int number1, int number2){
-        Console.WriteLine($"Your result: {number1} + {number2} = " + (number1 + number2));
-    }
 }
